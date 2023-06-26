f652850c1894c6fc0cdac21db4bc433590c67aec
Mouryou <yojeich@gmail.com>
2023-06-26 01:55:52 -0700

    Initial Commit

---
+++ IPA_Keyboard_iOS/IPA_Keyboard_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPA_Keyboard_iOSApp.swift
+//  IPA_Keyboard_iOS
+//
+//  Created by Shengtao Xu on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct IPA_Keyboard_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IPA_Keyboard_iOSUITests/IPA_Keyboard_iOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  IPA_Keyboard_iOSUITestsLaunchTests.swift
+//  IPA_Keyboard_iOSUITests
+//
+//  Created by Shengtao Xu on 6/26/23.
+//
+
+import XCTest
+
+final class IPA_Keyboard_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
